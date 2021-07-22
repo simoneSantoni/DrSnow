8fb878be13620de5ec5c11af2dc68a06c63044ac
Simone Santoni <simone.santoni.1@city.ac.uk>
2021-07-22 14:27:42 +0100

    Update clean_bib_items.py

--- workingPapers/santoni-jacquart-sieweke-wp/companionCode/handleBibs/clean_bib_items.py
+++ workingPapers/santoni-jacquart-sieweke-wp/companionCode/handleBibs/clean_bib_items.py
@@ -7,24 +7,23 @@
 
 Author   : Simone Santoni, simone.santoni.1@city.ac.uk
 
-Synopsis : Sometimes, 'strange' carachters included in the abstract or other 
-           sections prevent BibLaTeX from compiling the bibliography. This script
-           gets rid of them.
+Synopsis : The script filters out BibTeX fields that are seldom used and
+           normalize the values included in the retained fields
 
-To do    : TODO: create classes to read and manipulate bilio entries
+To do    : None
 
 """
 
 # %% clean & write pipeline
 
 
-def clean_bib(in_f, out_f, common_words):
-    """[summary]
+def filter_and_norm(in_f, out_f, common_words):
+    """Filter and normalize BibTeX fields
 
     Args:
-        in_f ([type]): [description]
-        out_f ([type]): [description]
-        common_words ([type]): [description]
+        in_f (str): name of the file to clean
+        out_f (str): name of the file containing clean data
+        common_words (iterable): set of common words not to capitalize
     """
     # empty container
     refs = []
@@ -83,45 +82,8 @@ def clean_bib(in_f, out_f, common_words):
             pass
     # &
     for i, item in enumerate(refs):
-        refs[i] = refs[i].replace('&', '\&')
+        refs[i] = refs[i].replace("&", "\&")
     # write file
     with open(out_f, "w") as pipe:
         for item in refs:
             pipe.write(item)
-
-
-# %%
-def normalize_bib(in_f, out_f, common_words):
-    # empty container
-    refs = []
-    # read data
-    with open(in_f, "r") as pipe:
-        for line in pipe.readlines():
-                refs.append(line)
-    # item labels are all lower case
-    for i, item in enumerate(refs):
-        if "@article" in item:
-            refs[i] = item.lower()
-    for i, item in enumerate(refs):
-        if "@article" in item:
-            refs[i] = item.lower()
-    # escape '&'
-    for i, item in enumerate(refs):
-        refs[i] = refs[i].replace('&', '\&')
-    # item titles have 'title' case
-    for i, item in enumerate(refs):
-        if "title = " in item:
-            refs[i] = item.title()
-            for word in common_words:
-                refs[i] = refs[i].replace(word, word.lower())
-            for word in common_words:
-                old = ":{}".format(word.lower())
-                new = ":{}".format(word)
-                refs[i] = refs[i].replace(old, new)
-        else:
-            pass
-    # write file
-    with open(out_f, "w") as pipe:
-        for item in refs:
-            pipe.write(item)
-
